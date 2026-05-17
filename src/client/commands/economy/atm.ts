ead496897dcf6384f70a9483938cdf8ef5e87f07
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-17 01:10:47 +0000

    atm

--- src/client/commands/economy/atm.ts
+++ src/client/commands/economy/atm.ts
@@ -1,18 +1,25 @@
 import { Command } from '../../../structure/Command';
 import { CommandPayload } from '../../../types/global';
+import { SlashCommandBuilder } from 'discord.js';
 
 export default class AtmCommand extends Command {
     public constructor() {
         super({
             name: 'atm',
             aliases: ['bal', 'balance'],
+            slashCommandData: 
+                new SlashCommandBuilder()
+                    .setName('saldo')
+                    .setDescription('Mostra o saldo da carteira e do banco')
         });
     }
 
-    public async execute({ client, message }: CommandPayload) {
-        const amount = await client.db.get(`users.${message.author.id}.amount`) ?? 0;
-        const bank = await client.db.get(`users.${message.author.id}.bank`) ?? 0;
+    public async execute({ client, context }: CommandPayload) {
+        const author = this.getAuthor(context);
+        
+        const amount = await client.db.get(`users.${author.id}.amount`) ?? 0;
+        const bank = await client.db.get(`users.${author.id}.bank`) ?? 0;
 
-        message.reply('[💵] Saldo\n🪙・Carteira**' + amount + '** moedas.\n🏦・Banco**' + bank + '** moedas.');
+        this.reply(context, '[💵] Saldo\n🪙・Carteira**' + amount + '** moedas.\n🏦・Banco**' + bank + '** moedas.');
     }
 }
